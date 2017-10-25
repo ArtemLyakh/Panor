56d5ca14e51fc3f66e570088219ea0aa9dc87119
ArtemLyakh <artemm.lyakh@gmail.com>
2017-10-25 09:03:06 +0300

    init

---
+++ Panor/Pages/Core/Main.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Panor.Pages.Core
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Main : ContentPage
+    {
+        public Main()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
