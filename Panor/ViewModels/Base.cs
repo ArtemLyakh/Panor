d906143e6bb0fb1f89db34c1ee713f8bc5ae5c87
ArtemLyakh <artemm.lyakh@gmail.com>
2017-10-25 11:16:51 +0300

    Base View Model

---
+++ Panor/ViewModels/Base.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Panor.ViewModels
+{
+    public abstract class Base : INotifyPropertyChanged
+    {
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void SetProperty<T>(ref T storage, T value, Action callback = null, [CallerMemberName] string propertyName = null)
+        {
+            if (object.Equals(storage, value)) return;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            callback?.Invoke();
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    }
+}
