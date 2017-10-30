920931e11a536747dc6ccd16c709e42bf404085d
ArtemLyakh <artemm.lyakh@gmail.com>
2017-10-30 18:12:13 +0300

    StartPage

---
+++ Panor/Pages/Start.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Panor.Pages
+{
+    public partial class Start : BasePage
+    {
+        public Start()
+        {
+            InitializeComponent();
+        }
+    }
+}
