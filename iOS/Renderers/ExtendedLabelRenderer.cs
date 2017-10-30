6fadba0d2dfbe4b9bc721c5b92bb32b9d2d3c4fe
ArtemLyakh <artemm.lyakh@gmail.com>
2017-10-30 17:00:08 +0300

    ios underline label renderer

---
+++ iOS/Renderers/ExtendedLabelRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Panor.Views.ExtendedLabel), typeof(Panor.iOS.Renderers.ExtendedLabelRenderer))]
+namespace Panor.iOS.Renderers
+{
+    public class ExtendedLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            var extendedLabel = Element as Views.ExtendedLabel;
+
+            if (extendedLabel != null && extendedLabel.IsUnderline)
+            {
+                var text = (NSMutableAttributedString)Control.AttributedText;
+                var range = new NSRange(0, text.Length);
+                text.AddAttribute(UIStringAttributeKey.UnderlineStyle, NSNumber.FromInt32((int)NSUnderlineStyle.Single), range);
+            }
+
+        }
+    }
+}
