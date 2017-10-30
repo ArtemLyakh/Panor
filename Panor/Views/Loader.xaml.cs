3dddec767cedb9f551bdf5e25f5963667a369e29
ArtemLyakh <artemm.lyakh@gmail.com>
2017-10-30 14:04:33 +0300

    loader

---
+++ Panor/Views/Loader.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Panor.Views
+{
+    public partial class Loader : ContentView
+    {
+        public Loader()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        public static readonly BindableProperty IsActiveProperty = BindableProperty.Create(
+            nameof(Command),
+            typeof(bool),
+            typeof(AuthBlock),
+            false
+        );
+        public bool IsActive
+        {
+            set => SetValue(IsActiveProperty, value);
+            get => (bool)GetValue(IsActiveProperty);
+        }
+    }
+}
