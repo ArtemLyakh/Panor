2942f8694ce66e20c4132ac729533f93aa726ac8
ArtemLyakh <artemm.lyakh@gmail.com>
2017-11-07 15:23:04 +0300

    ios dropdown

--- Panor/Views/Dropdown.cs
+++ Panor/Views/Dropdown.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace Panor.Views
 {
-    public class Dropdown : View
+    public class Dropdown : ContentView
     {
         public static readonly BindableProperty CommandsProperty = BindableProperty.Create(
             nameof(Commands),
@@ -29,7 +30,30 @@ namespace Panor.Views
 
         private void CreateIOS()
         {
-            
+            var img = new Image()
+            {
+                Source = ImageSource.FromFile("dropdown_btn"),
+                Aspect = Aspect.Fill
+            };
+            var tgr = new TapGestureRecognizer();
+            tgr.Tapped += Tgr_Tapped;
+
+            img.GestureRecognizers.Add(tgr);
+
+            Content = img;
+        }
+
+        async void Tgr_Tapped(object sender, EventArgs e)
+        {
+            if (Commands == null) return;
+
+            var res = await App.Current.MainPage.DisplayActionSheet(null, "Отмена", null, Commands.Select(i => i.Text).ToArray());
+
+            var selectedCommand = Commands.FirstOrDefault(i => i.Text == res).Command;
+            if (selectedCommand != null && selectedCommand.CanExecute(null))
+            {
+                selectedCommand.Execute(null);
+            }
         }
     }
 }
