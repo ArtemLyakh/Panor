4dbf8e3c8361d62286fa40b3bcf26249e014d4aa
ArtemLyakh <artemm.lyakh@gmail.com>
2017-11-08 10:11:32 +0300

    latestnumbers to contentview + mainbanner start

---
+++ Panor/Views/MainBanner.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Panor.Views
+{
+    public partial class MainBanner : ContentView
+    {
+        public MainBanner()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty ViewStateProperty = BindableProperty.Create(
+            nameof(ViewState),
+            typeof(LoadingContentViewState),
+            typeof(MainBanner),
+            default(LoadingContentViewState)
+        );
+        public LoadingContentViewState ViewState
+        {
+            set => SetValue(ViewStateProperty, value);
+            get => (LoadingContentViewState)GetValue(ViewStateProperty);
+        }
+
+        public static readonly BindableProperty ReloadCommandProperty = BindableProperty.Create(
+            nameof(ReloadCommand),
+            typeof(ICommand),
+            typeof(MainBanner)
+        );
+        public ICommand ReloadCommand
+        {
+            set => SetValue(ReloadCommandProperty, value);
+            get => (ICommand)GetValue(ReloadCommandProperty);
+        }
+
+
+    }
+}
