bf528592d9ffd43cfb337a4b66287944c0a5613a
ArtemLyakh <artemm.lyakh@gmail.com>
2017-11-08 15:30:36 +0300

    free numbers template

---
+++ Panor/Views/Templates/MainPageFreeNumber.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Panor.Views.Templates
+{
+    public partial class MainPageFreeNumber : ContentView
+    {
+        public MainPageFreeNumber()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty ImageProperty = BindableProperty.Create(
+            nameof(Image),
+            typeof(ImageSource),
+            typeof(MainPageFreeNumber)
+        );
+        public ImageSource Image
+        {
+            set => SetValue(ImageProperty, value);
+            get => (ImageSource)GetValue(ImageProperty);
+        }
+
+        public static readonly BindableProperty TitleProperty = BindableProperty.Create(
+            nameof(Title),
+            typeof(string),
+            typeof(MainPageFreeNumber)
+        );
+        public string Title
+        {
+            set => SetValue(TitleProperty, value);
+            get => (string)GetValue(TitleProperty);
+        }
+
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(
+            nameof(Text),
+            typeof(string),
+            typeof(MainPageFreeNumber)
+        );
+        public string Text
+        {
+            set => SetValue(TextProperty, value);
+            get => (string)GetValue(TextProperty);
+        }
+
+    }
+}
