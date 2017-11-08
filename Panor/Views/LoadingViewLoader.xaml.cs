b02318be2b6318d52dd291013a4f891b5fdc0f22
ArtemLyakh <artemm.lyakh@gmail.com>
2017-11-08 10:24:31 +0300

    loading elements

---
+++ Panor/Views/LoadingViewLoader.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Panor.Views
+{
+    public partial class LoadingViewLoader : ContentView
+    {
+        public LoadingViewLoader()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty ViewStateProperty = BindableProperty.Create(
+            nameof(ViewState),
+            typeof(LoadingContentViewState),
+            typeof(LoadingViewLoader),
+            default(LoadingContentViewState)
+        );
+        public LoadingContentViewState ViewState
+        {
+            set => SetValue(ViewStateProperty, value);
+            get => (LoadingContentViewState)GetValue(ViewStateProperty);
+        }
+    }
+}
---
+++ Panor/Views/LoadingViewReload.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Panor.Views
+{
+    public partial class LoadingViewReload : ContentView
+    {
+        public LoadingViewReload()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty ViewStateProperty = BindableProperty.Create(
+            nameof(ViewState),
+            typeof(LoadingContentViewState),
+            typeof(MainBanner),
+            default(LoadingContentViewState)
+        );
+        public LoadingContentViewState ViewState
+        {
+            set => SetValue(ViewStateProperty, value);
+            get => (LoadingContentViewState)GetValue(ViewStateProperty);
+        }
+
+        public static readonly BindableProperty ReloadCommandProperty = BindableProperty.Create(
+            nameof(ReloadCommand),
+            typeof(ICommand),
+            typeof(MainBanner)
+        );
+        public ICommand ReloadCommand
+        {
+            set => SetValue(ReloadCommandProperty, value);
+            get => (ICommand)GetValue(ReloadCommandProperty);
+        }
+    }
+}
